f6acafca810f12c6ac1bba91aabbbf39c283e4df
Hitesh Tare <hiteshtare@gmail.com>
2018-12-11 13:05:14 +0530

    Added Search Textbox

--- server/api/controllers/fuzzyContoller.js
+++ server/api/controllers/fuzzyContoller.js
@@ -3,22 +3,21 @@ const fuzzball = require('fuzzball');
 
 const _ = require("lodash");
 
+choices = ["John", "Johnny", "Johnson", "Honson", "Bruce", "Kruze", "Catherine ", "Kathryn", "Zero", "Zee ro", "One", "Wun", "Two", "Too", "Three", "Tree", "Four", "Fou er", "Five", "Fife", "Six", "Six", "Seven", "Sev en", "Eight", "Ate", "Nine", "Crying", "Alfa", "Alpha", "Bravo", "Bra voh", "Charlie", "Char Lee", "Delta", "Del Tah", "Echo", "Eck Ho", "India", "In Dee Ah", "Juliet", "Jew Lee Ett", "Lima ", "Lee Mah", "Papa", "Pah Pah", "Romeo", "Row Me Oh", "Seirra", "See Air Rah", "Victor", "Vik Tah", "Whiskey ", "Wiss key", "Yankee", "Yang Key", "Zulu", "Zoo Loo"];
 
 exports.fuzzy_default = async (req, res, next) => {
   try {
 
     options = {
-      scorer: fuzzball.token_set_ratio
+      scorer: fuzzball.token_set_ratio, // any function that takes two values and returns a score, default: ratio
+      //processor: choice => choice.model, //takes choice object, returns string, default: no processor. Must supply if choices are not already strings.
+      //limit: 2, // max number of top results to return, default: no limit / 0.
+      //cutoff: 50, // lowest score to return, default: 0
+      //unsorted: false // results won't be sorted if true, default: false. If true limit will be ignored.
     };
-    choices = ["John", "Johnny", "Johnson", "Honson", "Bruce", "Kruze", "Catherine ", "Kathryn", "Zero", "Zee ro", "One", "Wun", "Two", "Too", "Three", "Tree", "Four", "Fou er", "Five", "Fife", "Six", "Six", "Seven", "Sev en", "Eight", "Ate", "Nine", "Crying", "Alfa", "Alpha", "Bravo", "Bra voh", "Charlie", "Char Lee", "Delta", "Del Tah", "Echo", "Eck Ho", "India", "In Dee Ah", "Juliet", "Jew Lee Ett", "Lima ", "Lee Mah", "Papa", "Pah Pah", "Romeo", "Row Me Oh", "Seirra", "See Air Rah", "Victor", "Vik Tah", "Whiskey ", "Wiss key", "Yankee", "Yang Key", "Zulu", "Zoo Loo"];
-
-    fuzzball.extract("Zoo", choices, options);
 
     // in supported environments, Promise will not be polyfilled
     fuzzball.extractAsPromised("Zoo", choices, options).then(fuzzy_res => { /* do stuff */
-      console.log(`fuzzy_res`);
-      console.log(fuzzy_res);
-
       res.status(200).json({
         success: true,
         message: 'Fuzzy default invoked successfully',
@@ -37,12 +36,21 @@ exports.fuzzy_default = async (req, res, next) => {
 
 exports.fuzzy_custom = async (req, res, next) => {
   try {
-    res.status(201).json({
-      success: true,
-      message: 'Fuzzy custom invoked successfully',
-      // payload: result
-    });
+    const searchStr = req.body.searchStr;
 
+    options = {
+      scorer: fuzzball.token_set_ratio,
+    };
+
+    // in supported environments, Promise will not be polyfilled
+    fuzzball.extractAsPromised(searchStr, choices, options).then(fuzzy_res => { /* do stuff */
+      res.status(200).json({
+        success: true,
+        message: 'Fuzzy custom invoked successfully',
+        payload: fuzzy_res
+      });
+
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json({
